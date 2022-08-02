e1f544c206b03dd630b0d46f6c9a9eea4215d43f
GoogleKhan <shadab105662@gmail.com>
2022-08-02 11:30:05 +0530

    Ignore H2

--- workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
+++ workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.greatlearning.security.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,13 +12,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder passwordEncoder =  getEncoder();
-		
-		auth.inMemoryAuthentication()
-		.passwordEncoder(passwordEncoder)
-		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
-		.and()
-		.withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
+		PasswordEncoder passwordEncoder = getEncoder();
+
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder).withUser("Adam")
+				.password(passwordEncoder.encode("1234")).roles("OWNER").and().withUser("Riya")
+				.password(passwordEncoder.encode("4321")).roles("CLERK");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
 	}
 
 	PasswordEncoder getEncoder() {
