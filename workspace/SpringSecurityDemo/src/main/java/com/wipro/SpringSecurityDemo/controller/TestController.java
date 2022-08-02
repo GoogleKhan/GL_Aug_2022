56d2ced3d8017b09706848f6e04968c5f47bbb49
GoogleKhan <shadab105662@gmail.com>
2022-08-02 10:41:28 +0530

    First Commit

---
+++ workspace/SpringSecurityDemo/src/main/java/com/wipro/SpringSecurityDemo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.wipro.SpringSecurityDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello, Good Morning";
+	}
+
+}
---
+++ workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.greatlearning.security.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder passwordEncoder =  getEncoder();
+		
+		auth.inMemoryAuthentication()
+		.passwordEncoder(passwordEncoder)
+		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
+		.and()
+		.withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
+	}
+
+	PasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
