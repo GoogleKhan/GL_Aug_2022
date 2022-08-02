db8a17bfd1752a506ea5da1df5e5abbe4e45565e
GoogleKhan <shadab105662@gmail.com>
2022-08-02 13:14:17 +0530

    Role based Authorisation

--- workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
+++ workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.greatlearning.security.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private DataSource dataSource;
 
@@ -23,10 +25,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
 //		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
 //		.and().withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
-		
-		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
-		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
-		.and().withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
+
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser("Adam")
+				.password(passwordEncoder.encode("1234")).roles("OWNER").and().withUser("Riya")
+				.password(passwordEncoder.encode("4321")).roles("CLERK");
 	}
 
 	@Override
@@ -34,6 +36,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers("/h2-console/**");
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/organicVeggies/makeAnnouncement", "/organicVeggies/viewFinancials").hasRole("OWNER")
+				.antMatchers("/organicVeggies/checkInventory", "/organicVeggies/viewInventory",
+						"/organicVeggies/doCheckout")
+				.hasAnyRole("OWNER", "CLERK").antMatchers("/").permitAll().and().formLogin();
+	}
+
+	@Bean
 	PasswordEncoder getEncoder() {
 		return new BCryptPasswordEncoder();
 	}
