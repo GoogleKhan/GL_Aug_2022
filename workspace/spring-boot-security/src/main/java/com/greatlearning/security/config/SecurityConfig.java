8f49e14e1330eba9b026ddc77a186bb7852f24dc
GoogleKhan <shadab105662@gmail.com>
2022-08-02 11:52:12 +0530

    JDBC authentication

--- workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
+++ workspace/spring-boot-security/src/main/java/com/greatlearning/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.greatlearning.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,14 +12,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder passwordEncoder = getEncoder();
 
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder).withUser("Adam")
-				.password(passwordEncoder.encode("1234")).roles("OWNER").and().withUser("Riya")
-				.password(passwordEncoder.encode("4321")).roles("CLERK");
+//		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+//		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
+//		.and().withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
+		
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+		.withUser("Adam").password(passwordEncoder.encode("1234")).roles("OWNER")
+		.and().withUser("Riya").password(passwordEncoder.encode("4321")).roles("CLERK");
 	}
 
 	@Override
